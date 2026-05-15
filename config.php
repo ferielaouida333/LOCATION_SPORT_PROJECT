e0bf59499ab5d7301444f652573635003f6617b1
feriel <ferielaouida@gmail.com>
2026-05-15 15:30:12 +0100

    creation config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST',    'localhost');        
+define('DB_NAME',    'location_sport'); 
+define('DB_USER',    'root');           
+define('DB_PASS',    '');               
+define('DB_CHARSET', 'utf8');
+
